67b7b94fa27424ceccbc7ba174a2b33f6dea31d0
KaterynaKocheharova <kot41901@gmail.com>
2024-01-11 13:38:35 +0200

    task 2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,35 @@
-"use strict"
+'use strict';
+
+// ============================================== Задача 2. Склад ===============================================
+
+class Storage {
+  #items;
+  constructor(itemsArray) {
+    this.#items = itemsArray;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items.forEach(item => {
+      if (item === itemToRemove) {
+        this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+      }
+    });
+  }
+}
+
+// ============================================== Перевірка =====================================================
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
